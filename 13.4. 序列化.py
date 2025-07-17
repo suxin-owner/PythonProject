d8131bf80e54575f046fa0e23a79e382723d68be
suxin-owner <1356369289@qq.com>
2025-07-17 16:28:00 +0800

    a

--- 13.4. 序列化.py
+++ 13.4. 序列化.py
@@ -27,7 +27,11 @@ class Student(object):
         self.name = name
         self.age = age
         self.score = score
+    
+    def __str__(self):
+        return "Student object (%s, %s, %s)" % (self.name, self.age, self.score)
 
+# 序列化方法一
 def student2dict(stu):
     return {
     'name': stu.name,
@@ -35,5 +39,19 @@ def student2dict(stu):
     'score': stu.score
     }
 
-s = Student('Bob', 30, 88)
-print(json.dumps(s, default=student2dict))
+s1 = Student('Bob', 30, 88)
+std1 = json.dumps(s1, default=student2dict)
+print(std1)
+
+# 序列化方法二:匿名函数写法
+s2 = Student('Bob', 20, 100)
+std2 = json.dumps(s2, default=lambda x : x.__dict__)
+print(std2)
+
+# 将json str 反序列化成一个对象
+# def dict2student(d):
+#     return Student(d['name'], d['age'], d['score'])
+
+json_str = '{"age": 20, "score": 88, "name": "Bob"}'
+s3 = json.loads(json_str, object_hook=lambda d : Student(d['name'], d['age'], d['score']))
+print(s3)
---
+++ 14.1. 多进程.py
@@ -0,0 +1,54 @@
+# import os
+
+# print('Process (%s) start...' % os.getpid())
+# # Only works on Unix/Linux/macOS:
+# pid = os.fork()
+# if pid == 0:
+#     print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+# else:
+#     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+
+
+# from multiprocessing import Process
+# import os
+
+# # 子进程要执行的代码
+# def run_proc(name):
+#     print('Run child process %s (%s)...' % (name, os.getpid()))
+
+# if __name__=='__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Process(target=run_proc, args=('test',))      #创建一个子进程
+#     print('Child process will start.')
+#     p.start()  #启动进程
+#     p.join()   #等待进程结束
+#     print('Child process end.')
+
+# # pool
+# from multiprocessing import Pool
+# import os, time, random
+
+# def long_time_task(name):
+#     print('Run task %s (%s)...' % (name, os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random() * 3) # 0-3 之间随机浮点数
+#     end = time.time()
+#     print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+# if __name__=='__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Pool(4)
+#     for i in range(5):
+#         p.apply_async(long_time_task, args=(i,))
+#     print('Waiting for all subprocesses done...')
+#     p.close()
+#     p.join()
+#     print('All subprocesses done.')
+
+# 子进程 subprocess
+
+import subprocess
+
+print('$ nslookup www.python.org')
+r = subprocess.call(['nslookup', 'www.python.org'])
+print('Exit code:', r)
