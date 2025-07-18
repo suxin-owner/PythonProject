8db1261d67a1fbec1b10467dc5197443fd74c3dd
suxin-owner <1356369289@qq.com>
2025-07-18 16:44:05 +0800

    a

---
+++ 14.5. 分布式进程master.py
@@ -0,0 +1,53 @@
+# task_master.py
+
+import random, time, os, queue
+from multiprocessing.managers import BaseManager
+
+# 发送任务的队列:
+task_queue = queue.Queue()  
+# 接收结果的队列:
+result_queue = queue.Queue() 
+
+def get_task_queue():
+    return task_queue
+
+def get_result_queue():
+    return result_queue
+
+# 从BaseManager继承的QueueManager:
+class QueueManager(BaseManager):
+    pass
+
+# 把两个Queue都注册到网络上, callable参数关联了Queue对象:
+# QueueManager.register('get_task_queue', callable=lambda: task_queue)
+# QueueManager.register('get_result_queue', callable=lambda: result_queue)
+QueueManager.register('get_task', callable=get_task_queue)
+QueueManager.register('get_result', callable=get_result_queue)
+
+if __name__ == '__main__':
+    # 绑定端口5000, 设置验证码'abc':
+    manager = QueueManager(address=('127.0.0.1', 5000), authkey=b'abc')
+
+    # 启动Queue:
+    manager.start()
+    print('Parent process %s.' % os.getpid())
+
+    # 获得通过网络访问的Queue对象:
+    task = manager.get_task()
+    result = manager.get_result()
+
+    # 放几个任务进去:
+    for i in range(10):
+        n = random.randint(0, 10000)
+        print('Put task %d...' % n)
+        task.put(n)
+
+    # 从result队列读取结果:
+    print('Try get results...')
+    for i in range(10):
+        r = result.get(timeout=10000)
+        print('Result: %s' % r)
+        
+    # 关闭:
+    manager.shutdown()
+    print('master exit.')
