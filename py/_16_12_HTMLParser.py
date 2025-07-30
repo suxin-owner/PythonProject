2df2994d445c33158df75d8e471f30751248a02e
suxin-owner <1356369289@qq.com>
2025-07-30 13:51:32 +0800

    a

--- py/_16_12_HTMLParser.py
+++ py/_16_12_HTMLParser.py
@@ -40,7 +40,11 @@
 #     </time>''')
 
 
-
+#---------------------------------------------------------------------------------#
+# 练习
+# 找一个网页，例如https://www.python.org/events/python-events/，
+# 用浏览器查看源码并复制，然后尝试解析一下HTML，输出Python官网发布的会议时间、名称和地点。
+#---------------------------------------------------------------------------------#
 from html.parser import HTMLParser
 from urllib.request import Request,urlopen
 import re, gzip, io
@@ -59,7 +63,6 @@ def get_data(url):
       data = f.read()
       raw = gzip.GzipFile(fileobj=io.BytesIO(data)).read()
       print(f'Status: {f.status} {f.reason}')
-      print()
       # 查看服务器返回的是不是压缩流
       print('---------------------------------------------------------------------')
       print(f.headers.get('Content-Encoding'))
@@ -69,48 +72,65 @@ def get_data(url):
 class MyHTMLParser(HTMLParser):
    def __init__(self):
       super().__init__()
-      self.__parsedata = 'name' # 设置一个空的标志位
+      self.__parsedata_name = False # 设置一个空的标志位
+      self.__parsedata_time = False
+      self.__parsedata_year = False
+      self.__parsedata_location = False
       self.info = []
+      self.time = []
+      self.year = []
+      self.num = 0
 
    def handle_starttag(self, tag, attrs):
-    #   if ('class', 'event-title') in attrs:
-    #      self.__parsedata = 'name'  # 通过属性判断如果该标签是我们要找的标签，设置标志位
+      print(attrs)
+      if ('class', 'event-title') in attrs:
+         self.__parsedata_name = True  # 通过属性判断如果该标签是我们要找的标签，设置标志位
       if tag == 'time':
-         self.__parsedata = 'time'
-    #   if ('class', 'say-no-more') in attrs:
-    #      self.__parsedata = 'year'
-    #   if ('class', 'event-location') in attrs:
-    #      self.__parsedata = 'location'
+         self.__parsedata_time = True
+      if ('class', 'say-no-more') in attrs:
+         self.__parsedata_year = True
+      if ('class', 'event-location') in attrs:
+         self.__parsedata_location = True
 
    def handle_endtag(self, tag):
       if tag == 'time':
-        self.__parsedata = ''# 在HTML 标签结束时，把标志位清空
+        self.__parsedata_time = False    # 在HTML 标签结束时，把标志位清空
+        result = ''.join(self.time).replace('.', '')
+        self.info.append(f'会议时间:{result}')
+        self.time.clear()
+        self.info.append(f'会议年份:{self.year[0]}')
+        self.year.clear()
 
+      self.__parsedata_name = False
+      self.__parsedata_year = False
+      self.__parsedata_location = False
+      
    def handle_data(self, data):
+      if self.__parsedata_name :
+         # 通过标志位判断，输出打印标签内容
+         self.num += 1
+         self.info.append(f'会议号: %5d' % self.num)
+         self.info.append(f'会议名称:{data}')
 
-    #   if self.__parsedata == 'name':
-    #      # 通过标志位判断，输出打印标签内容
-    #      self.info.append(f'会议名称:{data}')
+      if self.__parsedata_time :
+         if data.strip() != '2025' :
+            self.time.append(data.strip().replace('–\n            ', '-'))
 
-      if self.__parsedata == 'time':
-         print(data)
-         self.info.append(f'会议时间:{data}')
+      if self.__parsedata_year :
+        if re.match(r'\d{4}', data.strip()): # 因为后面还有两组 say-no-more 后面的data却不是年份信息,所以用正则检测一下
+            self.year.append(data.strip())
 
-    #   if self.__parsedata == 'year':
-    #      if re.match(r'\s\d{4}', data): # 因为后面还有两组 say-no-more 后面的data却不是年份信息,所以用正则检测一下
-    #         self.info.append(f'会议年份:{data}')
-
-    #   if self.__parsedata == 'location':
-    #      self.info.append(f'会议地点:{data} \n')
+      if self.__parsedata_location :
+         self.info.append(f'会议地点:{data} \n')
 
 parser = MyHTMLParser()
 URL = 'https://www.python.org/events/python-events'
-# URL = 'https://www.baidu.com'
 data = get_data(URL)
+parser.feed(data)
+for s in parser.info:
+    print(s)
+
 with open('/Users/suxin/Desktop/Self/Python/PythonProject/HTMLfile_16_12.txt','w') as f:
     f.write(data)
-parser.feed(data)
-# for s in parser.info:
-#     print(s)
 
 
--- py/test.py
+++ py/test.py
@@ -1,57 +1,30 @@
-from html.parser import HTMLParser
-from urllib.request import Request,urlopen
-import re, gzip, io
+from bs4 import BeautifulSoup
 
-def get_data(url):
-   '''
-   GET请求到指定的页面
-   :return: HTTP响应
-   '''
+html_content = '''
+<time datetime="2025-08-13T00:00:00+00:00">13 Aug.
+    <span id="start-2077" class="say-no-more">
+        2025
+    </span>
+     &ndash;
+        14 Aug.
+    <span id="end-2077" class="say-no-more">
+        2025
+    </span>
+</time>
+'''
 
-   headers = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'
-      }
-   req = Request(url, headers=headers)
-   with urlopen(req, timeout=25) as f:
-      data = f.read()
-      raw = gzip.GzipFile(fileobj=io.BytesIO(data)).read()
-      print(f'Status: {f.status} {f.reason}')
-      print()
-      # 查看服务器返回的是不是压缩流
-      print('---------------------------------------------------------------------')
-      print(f.headers.get('Content-Encoding'))
-      print('---------------------------------------------------------------------')
-      return raw.decode("utf-8")
+# 解析 HTML
+soup = BeautifulSoup(html_content, 'html.parser')
 
-class MyParser(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.in_time = False
-        self.pieces = []
+# 找到 <time> 标签
+time_tag = soup.find('time')
 
-    def handle_starttag(self, tag, attrs):
-        if tag == 'time':
-            self.in_time = True
+# 提取日期范围
+date_parts = []
+for element in time_tag.contents:
+    if element.name != 'span':
+        date_parts.append(element.strip())
 
-    def handle_endtag(self, tag):
-        if tag == 'time':
-            self.in_time = False
-
-    def handle_data(self, data):
-        if self.in_time:
-            # 去掉首尾空白并过滤纯空白文本
-            piece = data.strip()
-            print(piece)
-            if piece:
-                # 把 2025 去掉，只保留月份和日期
-                if '2025' not in piece:
-                    self.pieces.append(piece)
-
-parser = MyParser()
-URL = 'https://www.python.org/events/python-events'
-data = get_data(URL)
-parser.feed(data)
-
-# 拼接得到 '13 Aug.–14 Aug.'，再统一去掉句点
-result = ''.join(parser.pieces).replace('.', '')
-print(result)        # -> 13 Aug–14 Aug
+# 拼接日期范围
+date_range = ' '.join(date_parts)
+print(date_range)  # 输出: 13 Aug. – 14 Aug.
